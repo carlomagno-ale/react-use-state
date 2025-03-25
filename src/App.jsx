35b39af426bb69a956ebaa1250a9c8f86fa67849
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-25 17:24:18 +0100

    add Bootstrap integration and create Main component for educational content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import Main from './components/Esercizio.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Main />
     </>
   )
 }
